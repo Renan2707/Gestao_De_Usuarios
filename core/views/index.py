ba47bb86600ebf4170f6cd9e005d75ce4438fab1
renan <renancarloss27@gmail.com>
2026-02-12 19:32:49 -0300

    passando usuarios para o context na view

--- core/views/index.py
+++ core/views/index.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from core.models.usuario import Usuario
 
 def index(request):
-    return render(request, 'index.html')
+    usuarios = Usuario.objects.all()
+    usuarios_ativos = usuarios.filter(status=True).count()
+    usuarios_inativos = usuarios.filter(status=False).count()
+    return render(request, 'index.html', {'usuarios': usuarios, 'usuarios_ativos': usuarios_ativos, 'usuarios_inativos': usuarios_inativos})
