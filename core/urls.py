437cea0d9359466d2575c55bd944460f0cfe11a0
renan <renancarloss27@gmail.com>
2026-02-12 19:31:06 -0300

    ajuste de url

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from .views.user import add_user, edit_user, remove_user
 
 urlpatterns = [
     path('',index, name='index'),
-    path('add-user/', add_user, name='add_user'),
+    path('add_user/', add_user, name='add_user'),
     path('edit-user/<int:user_id>/', edit_user, name='edit_user'),
     path('remove-user/<int:user_id>/', remove_user, name='remove_user'),
 ]
